611a2af2ccf3d9ee1d4a2c907da0119d11949913
Ben Forde <forbe404.otago.co.nz>
2024-01-06 14:50:07 +1300

    Initial commit

---
+++ test/beanstalk_attempt_cdk.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const BeanstalkAttemptCdk = require('../lib/beanstalk_attempt_cdk-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/beanstalk_attempt_cdk-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new BeanstalkAttemptCdk.BeanstalkAttemptCdkStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
